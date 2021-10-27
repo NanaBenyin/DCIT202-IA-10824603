ab5e934fbe0f232abcdaebea2a72ebeffced70a3
NanaBenyin <66997771+NanaBenyin@users.noreply.github.com>
2021-10-26 18:06:06 -0700

    Update Login.js

--- bikeapp/screens/Login.js
+++ bikeapp/screens/Login.js
@@ -1,6 +1,14 @@
-import React from "react";
-import { View, Text, Image, TextInput, TouchableOpacity } from "react-native";
-import { AntDesign } from "@expo/vector-icons";
+import React from 'react';
+import {
+  View,
+  Text,
+  Image,
+  TextInput,
+  TouchableOpacity,
+  ImageBackground,
+  StyleSheet,
+} from 'react-native';
+import { AntDesign } from '@expo/vector-icons';
 import { setCustomText } from 'react-native-global-props';
 
 export default function Login(params) {
@@ -8,97 +16,69 @@ export default function Login(params) {
   return (
     <View
       style={{
-        backgroundColor: "white",
         flex: 1,
         paddingHorizontal: 20,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Image
-        style={{
-          transform: [{ rotate: "45deg" }],
-          borderRadius: 20,
-          marginBottom: 60,
-          width: 200,
-          height: 200,
-        }}
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+      <ImageBackground
         source={{
-          uri: "https://images.unsplash.com/photo-1623291638878-e2cdc1b77245?ixid=MnwxMjA3fDB8MHxzZWFyY2h8OTl8fGJpa2VzfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+          uri: 'https://images.unsplash.com/photo-1533139502658-0198f920d8e8?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDd8fHRocm93aW5nJTIwYSUyMHdhdGNofGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
         }}
-      />
-      <Text style={{ fontSize: 30, color: "grey" }}>Welcome to</Text>
-      <Text style={{ fontSize: 40, fontWeight: "bold" }}>Power Bike Shop</Text>
-      {/* <View style={{}}>
-        <TextInput
-          style={{
-            padding: 15,
-            borderRadius: 10,
-            borderWidth: 0.7,
-            paddingHorizontal: 60,
-          }}
-          placeholder="Enter your username"
-        />
-      </View> */}
-      {/* <View style={{ marginTop: 10,}}>
-        <TextInput
-          secureTextEntry={true}
-          onChangeText={(
-            e
-          )=> {
-            console.log(e)
-          }}
-          style={{
-            padding: 15,
-            borderRadius: 10,
-            borderWidth: 0.7,
-            paddingHorizontal: 60,
-          }}
-          placeholder="Enter your password"
-        />
-      </View> */}
+        resizeMode="cover"
+        style={styles.image}>
+        <Text style={styles.text}>Watchly</Text>
+        <Text style={{textAlign:"center", color:"#fff", fontSize:15}}>Welcome to watchly.world.com  {'\n'}
+        Where you find the best fit for your wrist!</Text>
+      </ImageBackground>
+
       <TouchableOpacity
         onPress={() => {
-          navigation.navigate("Home");
+          navigation.navigate('Home');
         }}
         style={{
           padding: 10,
-          paddingHorizontal: 70,
-          marginTop: 10,
-          alignItems: "center",
+          paddingHorizontal: 95,
+          marginBottom: 10,
+          alignItems: 'center',
           borderRadius: 10,
-          flexDirection: "row",
-          backgroundColor: "#e6e6e6",
-        }}
-      >
-        
-        <Image style={{height: 24, width: 24}} source={require('./google-icon.png')}/>
-        <Text style={{ paddingLeft: 10, color: "black" }}>Login with Gmail</Text>
-        
-      </TouchableOpacity>
-       <TouchableOpacity
-        onPress={() => {
-          navigation.navigate("Home");
-        }}
-        style={{
-          padding: 10,
-          paddingHorizontal: 70,
-          marginTop: 10,
-          alignItems: "center",
-          borderRadius: 10,
-          flexDirection: "row",
-          backgroundColor: "black",
-        }}
-      > 
-        <AntDesign name="apple1" size={24} color="white" />
-        <Text style={{ paddingLeft: 10, color: "white" }}>Login with Apple</Text>
+          flexDirection: 'row',
+          backgroundColor: '#fff',
+        }}>
+        <Text style={{ paddingLeft: 10, color: '#000', fontWeight: 500, fontSize:20 }}>
+          Login
+        </Text>
       </TouchableOpacity>
 
       <TouchableOpacity>
-      <Text style={{ fontSize: 10, marginTop: 10, fontWeight: "500", color: "grey",  }}>Not a member? {" "}
-      <Text style={{color:"orange", fontWeight: "bold"}}>Sign Up</Text>
-      </Text>
+        <Text
+          style={{
+            fontSize: 15,
+            marginBottom: 10,
+            fontWeight: '700',
+            color: 'white',
+          }}>
+          Don't have an account?{' '}
+          <Text style={{ color: '#f0b88d', fontWeight: 'bold' }}>Sign Up</Text>
+        </Text>
       </TouchableOpacity>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  image: {
+    flex: 1,
+    justifyContent: 'center',
+    width: 500,
+    height: 1000,
+  },
+  text: {
+    color: 'white',
+    fontSize: 50,
+    lineHeight: 84,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    // backgroundColor: '#000000c0',
+  },
+});
