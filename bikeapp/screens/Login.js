e082cbb598ada927aed01976f63ccc1b20ae9e3b
NanaBenyin <66997771+NanaBenyin@users.noreply.github.com>
2021-10-29 13:19:10 -0700

    Update Login.js

--- bikeapp/screens/Login.js
+++ bikeapp/screens/Login.js
@@ -10,6 +10,10 @@ import {
 } from 'react-native';
 import { AntDesign } from '@expo/vector-icons';
 import { setCustomText } from 'react-native-global-props';
+import {
+  widthPercentageToDP as width1,
+  heightPercentageToDP as height1,
+} from 'react-native-responsive-screen';
 
 export default function Login(params) {
   const navigation = params.navigation;
@@ -17,49 +21,62 @@ export default function Login(params) {
     <View
       style={{
         flex: 1,
-        paddingHorizontal: 20,
+        // paddingHorizontal: 20,
         justifyContent: 'center',
         alignItems: 'center',
       }}>
-      <ImageBackground
-        source={{
-          uri: 'https://images.unsplash.com/photo-1533139502658-0198f920d8e8?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDd8fHRocm93aW5nJTIwYSUyMHdhdGNofGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
-        }}
-        resizeMode="cover"
-        style={styles.image}>
-        <Text style={styles.text}>Watchly</Text>
-        <Text style={{textAlign:"center", color:"#fff", fontSize:15}}>Welcome to watchly.world.com  {'\n'}
-        Where you find the best fit for your wrist!</Text>
-      </ImageBackground>
+      <View style={{ alignItems: 'left', justifyContent: 'left' }}>
+        <Text
+          style={{
+            color: '#333',
+            fontSize: 25,
+            fontWeight: 'bold',
+            marginRight: 10,
+          }}>
+          Welcome to
+        </Text>
+        <Text style={{ color: '#fd9843', fontSize: 40, fontWeight: 'bold' }}>
+          <Text style={{ color: '#e47112', fontWeight: 'bold' }}>watchly</Text>
+          .world.org
+        </Text>
+      </View>
+      <View style={styles.container1}>
+        <Image
+          source={require('./Images/watch-get-started.png')}
+          style={{
+            alignSelf: 'center',
+            width: 200,
+            height: 300,
+            marginBottom: 100,
+          }}
+        />
+      </View>
 
+
+      <Text style={{color: '#333', fontWeight: 'bold', textAlign: 'center'}}>“A gentleman’s choice of timepiece says as much about him as does his Saville Row suit.”</Text>
       <TouchableOpacity
         onPress={() => {
           navigation.navigate('Home');
         }}
         style={{
-          padding: 10,
-          paddingHorizontal: 95,
-          marginBottom: 10,
+          alignSelf: 'center',
+          paddingVertical: 10,
+          paddingHorizontal: 70,
+          width: 290,
+          marginTop: 30,
           alignItems: 'center',
           borderRadius: 10,
           flexDirection: 'row',
-          backgroundColor: '#fff',
+          backgroundColor: '#fd9843',
         }}>
-        <Text style={{ paddingLeft: 10, color: '#000', fontWeight: 500, fontSize:20 }}>
-          Login
-        </Text>
-      </TouchableOpacity>
-
-      <TouchableOpacity>
         <Text
           style={{
-            fontSize: 15,
-            marginBottom: 10,
-            fontWeight: '700',
             color: 'white',
+            alignSelf: 'center',
+            textAlign: 'center',
+            fontSize: 23,
           }}>
-          Don't have an account?{' '}
-          <Text style={{ color: '#f0b88d', fontWeight: 'bold' }}>Sign Up</Text>
+          Join us Now
         </Text>
       </TouchableOpacity>
     </View>
@@ -67,18 +84,13 @@ export default function Login(params) {
 }
 
 const styles = StyleSheet.create({
-  image: {
-    flex: 1,
-    justifyContent: 'center',
-    width: 500,
-    height: 1000,
-  },
-  text: {
-    color: 'white',
-    fontSize: 50,
-    lineHeight: 84,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    // backgroundColor: '#000000c0',
+  container1: {
+    backgroundColor: '#ffa94d',
+    height: height1('35%'),
+    width: width1('74%'),
+    alignSelf: 'center',
+    marginTop: 120,
+    marginBottom: 90,
+    borderRadius: 500,
   },
 });
