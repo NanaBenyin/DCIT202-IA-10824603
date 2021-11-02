dacf92d543dbcfba511e7d8906ce9ace3fc6106c
NanaBenyin <66997771+NanaBenyin@users.noreply.github.com>
2021-11-02 15:44:51 -0700

    Update Home.js

--- bikeapp/screens/Home.js
+++ bikeapp/screens/Home.js
@@ -3,7 +3,7 @@ import { View, Text, TouchableOpacity, Image } from 'react-native';
 import { Ionicons, FontAwesome, AntDesign } from '@expo/vector-icons';
 
 export default function Home(params) {
-   const navigation = params.navigation;
+  const navigation = params.navigation;
   return (
     <View
       style={{
@@ -18,27 +18,26 @@ export default function Home(params) {
           justifyContent: 'space-between',
           alignItems: 'center',
         }}>
-        <Ionicons name="ios-menu-outline" size={24} color="black" />
-        <FontAwesome name="motorcycle" size={24} color="black" />
+        <Ionicons name="ios-menu-outline" size={30} color="#000" />
+       
         <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
-          <AntDesign name="search1" size={20} color="black" />
           <Ionicons
             style={{ marginLeft: 15 }}
-            name="notifications-outline"
-            size={20}
-            color="black"
+            name="cart-outline"
+            size={30}
+            color="#000"
           />
         </View>
       </View>
 
-      <Text style={{ marginTop: 15, color: 'grey' }}>
-        The World's{' '}
-        <Text style={{ color: 'orange', fontSize: 18, fontWeight: 'bolder' }}>
-          Best Bike
+      <Text style={{ marginTop: 18, color: 'grey' }}>
+        Discover the world's{' '}
+        <Text style={{ color: '#ffa94d', fontSize: 20, fontWeight: 'bold' }}>
+          Best Watches
         </Text>{' '}
       </Text>
 
-      <Text style={{ fontWeight: 'boldest', marginTop: 20 }}>Categories</Text>
+      <Text style={{ fontWeight: 'boldest', marginBottom: 10, marginTop: 20}}>Categories</Text>
 
       <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
         <View
@@ -56,21 +55,21 @@ export default function Home(params) {
         <View
           style={{ backgroundColor: '#f4f2f2', padding: 10, borderRadius: 12 }}>
           <TouchableOpacity>
-            <Text style={{ color: '#828080' }}>Roadbike</Text>
+            <Text style={{ color: '#828080' }}>We Recommend</Text>
           </TouchableOpacity>
         </View>
 
         <View
           style={{ backgroundColor: '#f4f2f2', padding: 10, borderRadius: 12 }}>
           <TouchableOpacity>
-            <Text style={{ color: '#828080' }}>Mountain</Text>
+            <Text style={{ color: '#828080' }}>Popular</Text>
           </TouchableOpacity>
         </View>
 
         <View
           style={{ backgroundColor: '#f4f2f2', padding: 10, borderRadius: 12 }}>
           <TouchableOpacity>
-            <Text style={{ color: '#828080' }}>Urban</Text>
+            <Text style={{ color: '#828080' }}>Trending</Text>
           </TouchableOpacity>
         </View>
       </View>
@@ -81,7 +80,7 @@ export default function Home(params) {
             style={{
               marginRight: 20,
               backgroundColor: '#f4f2f2',
-              width: 150,
+              width: 160,
               height: 260,
               borderRadius: 25,
               padding: 15,
@@ -93,42 +92,82 @@ export default function Home(params) {
                 justifyContent: 'center',
                 alignSelf: 'center',
               }}
-              source={require('./Images/watch.png')}
+              source={require('./Images/wa-4.png')}
             />
             <Text style={{ color: '#828080', textAlign: 'center' }}>
-              Pinarello Bike
+              Schaffhausen{' '}
             </Text>
             <Text style={{ fontWeight: 'bolder', textAlign: 'center' }}>
               <Text style={{ color: 'orange', textAlign: 'center' }}>$</Text>{' '}
               1,700.00
             </Text>
+           <View
+              style={{
+                marginTop: 15,
+                backgroundColor: '#fd9843',
+                alignSelf: 'center',
+                borderRadius: 10,
+                paddingVertical: 5,
+                paddingHorizontal: 20,
+              }}>
+              <TouchableOpacity>
+                <Text
+                  style={{
+                    color: '#fff',
+                    fontWeight: 'bold',
+                    textAlign: 'center', 
+                  }}>
+                  Add to Cart
+                </Text>
+              </TouchableOpacity>
+            </View>
           </View>
         </View>
         <View style={{ marginTop: 30 }}>
           <View
             style={{
               backgroundColor: '#f4f2f2',
-              width: 200,
+              width: 160,
               height: 260,
               borderRadius: 25,
               padding: 15,
             }}>
             <Image
               style={{
-                height: 170,
-                width: 100,
+                height: 150,
+                width: 90,
                 justifyContent: 'center',
                 alignSelf: 'center',
               }}
-              source={require('./Images/wa-1.png')}
+              source={require('./Images/wa-5.png')}
             />
             <Text style={{ color: '#828080', textAlign: 'center' }}>
-              Pinarello Bike
+              DD Classic Watch
             </Text>
             <Text style={{ fontWeight: 'bolder', textAlign: 'center' }}>
               <Text style={{ color: 'orange', textAlign: 'center' }}>$</Text>{' '}
               1,700.00
             </Text>
+            <View
+              style={{
+                marginTop: 15,
+                backgroundColor: '#fd9843',
+                alignSelf: 'center',
+                borderRadius: 10,
+                paddingVertical: 5,
+                paddingHorizontal: 20,
+              }}>
+              <TouchableOpacity>
+                <Text
+                  style={{
+                    color: '#fff',
+                    fontWeight: 'bold',
+                    textAlign: 'center', 
+                  }}>
+                  Add to Cart
+                </Text>
+              </TouchableOpacity>
+            </View>
           </View>
         </View>
       </View>
@@ -138,7 +177,7 @@ export default function Home(params) {
           <View
             style={{
               backgroundColor: '#f4f2f2',
-              width: 200,
+              width: 160,
               height: 260,
               borderRadius: 25,
               padding: 15,
@@ -146,47 +185,87 @@ export default function Home(params) {
             }}>
             <Image
               style={{
-                height: 170,
-                width: 100,
+                height: 150,
+                width: 90,
                 justifyContent: 'center',
                 alignSelf: 'center',
               }}
               source={require('./Images/wa-2.png')}
             />
             <Text style={{ color: '#828080', textAlign: 'center' }}>
-              Pinarello Bike
+              Daniel Wellington
             </Text>
             <Text style={{ fontWeight: 'bolder', textAlign: 'center' }}>
               <Text style={{ color: 'orange', textAlign: 'center' }}>$</Text>{' '}
               1,700.00
             </Text>
+           <View
+              style={{
+                marginTop: 15,
+                backgroundColor: '#fd9843',
+                alignSelf: 'center',
+                borderRadius: 10,
+                paddingVertical: 5,
+                paddingHorizontal: 20,
+              }}>
+              <TouchableOpacity>
+                <Text
+                  style={{
+                    color: '#fff',
+                    fontWeight: 'bold',
+                    textAlign: 'center', 
+                  }}>
+                  Add to Cart
+                </Text>
+              </TouchableOpacity>
+            </View>
           </View>
         </View>
         <View style={{ marginTop: 30 }}>
           <View
             style={{
               backgroundColor: '#f4f2f2',
-              width: 200,
+              width: 160,
               height: 260,
               borderRadius: 25,
               padding: 15,
             }}>
             <Image
               style={{
-                height: 170,
-                width: 100,
+                height: 150,
+                width: 90,
                 justifyContent: 'center',
                 alignSelf: 'center',
               }}
-              source={require('./Images/wa-3.png')}
+              source={require('./Images/wa-2.png')}
             />
             <Text style={{ color: '#828080', textAlign: 'center' }}>
-              Pinarello Bike
+              St Mawes Watch
             </Text>
             <Text style={{ fontWeight: 'bolder', textAlign: 'center' }}>
               <Text style={{ color: 'orange', textAlign: 'center' }}>$</Text>{' '}
-              1,700.00
+              160.00
             </Text>
+            <View
+              style={{
+                marginTop: 15,
+                backgroundColor: '#fd9843',
+                alignSelf: 'center',
+                borderRadius: 10,
+                paddingVertical: 5,
+                paddingHorizontal: 20,
+              }}>
+              <TouchableOpacity>
+                <Text
+                  style={{
+                    color: '#fff',
+                    fontWeight: 'bold',
+                    textAlign: 'center', 
+                  }}>
+                  Add to Cart
+                </Text>
+              </TouchableOpacity>
+            </View>
           </View>
         </View>
       </View>
