e947af30153d94284458718182fa57cdb908735c
NanaBenyin <66997771+NanaBenyin@users.noreply.github.com>
2021-11-04 06:33:29 -0700

    Update App.js

--- bikeapp/App.js
+++ bikeapp/App.js
@@ -7,19 +7,24 @@ import { createStackNavigator } from "@react-navigation/stack";
 import Login from "./screens/Login";
 import Home from "./screens/Home";
 import Recommend from './screens/Recommend';
-import { theme } from './core/theme';
-import {StartScreen} from './screens/StartScreen';
-import{LoginScreen} from './screens/LoginScreen';
-import{RegisterScreen} from './screens/RegisterScreen';
-import{ResetPasswordScreen} from './screens/ResetPasswordScreen';
-import {Dashboard} from './screens/Dashboard';
+import Digital from './screens/Digital';
+import Analogue from './screens/Analogue';
+import { theme } from './core/theme'
+import {
+  StartScreen,
+  LoginScreen,
+  RegisterScreen,
+  ResetPasswordScreen,
+  Dashboard,
+} from './screens'
+
 
 export default function App() {
   const MainNavigator = createStackNavigator();
 
   return (
     <View style={{ flex: 1 }}>
-    <Provider theme={theme}>
+    
       <NavigationContainer>
         <MainNavigator.Navigator
           screenOptions={{ headerShown: false }}
@@ -28,14 +33,16 @@ export default function App() {
           <MainNavigator.Screen name="Login" component={Login} />
           <MainNavigator.Screen name="Home" component={Home} />
           <MainNavigator.Screen name="Recommend" component={Recommend} />
+          <MainNavigator.Screen name="Digital" component={Digital} />
+          <MainNavigator.Screen name="Analogue" component={Analogue} />
+          <MainNavigator.Screen name="StartScreen" component={StartScreen} />
           <MainNavigator.Screen name="LoginScreen" component={LoginScreen} />
-          <MainNavigator.Screen name="RegisterScreen" component={RegisterScreen} />
-           <MainNavigator.Screen name="StartScreen" component={StartScreen} />
           <MainNavigator.Screen name="ResetPasswordScreen" component={ResetPasswordScreen} />
-            <MainNavigator.Screen name="Dashboard" component={Dashboard} />
+          <MainNavigator.Screen name="Dashboard" component={Dashboard} />
+          
         </MainNavigator.Navigator>
       </NavigationContainer>
-      </Provider>
+      
     </View>
   );
 }
